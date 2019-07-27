7889df91f18f1d50f56ae47f6238a7ac4d71deed
horchliam <53147246+horchliam@users.noreply.github.com>
2019-07-27 13:16:19 -0400

    Add files via upload

---
+++ lebowski_extraction.py
@@ -0,0 +1,29 @@
+import cv2
+import os
+
+def frameCapture(path):
+    video = cv2.VideoCapture(path)
+    count = 0
+    fileNum = 0
+    success = True
+
+    while success:
+        success, image = video.read()
+        if(count % 200 == 0):
+            name = 'frame%d.jpg' % fileNum
+            print('uploading ...' + name)
+            cv2.imwrite(name, image)
+            fileNum += 1
+        count += 1
+
+def clearFolder(path):
+    for file in os.listdir(path):
+        os.remove(file)
+
+if not os.path.exists('unused_imgs'):
+    os.makedirs('unused_imgs')
+
+os.chdir('/Users/liamhorch/Desktop/my_bot_polished/unused_imgs')
+clearFolder('/Users/liamhorch/Desktop/my_bot_polished/unused_imgs')
+frameCapture('/Users/liamhorch/Desktop/my_bot_polished/Popcornarchive-theBigLebowski1998_rng96.mp4')
+print('Finished!')
