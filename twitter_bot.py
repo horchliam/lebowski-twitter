c4c057bd2d396f01e8a10a20733c2b98c3bf2f09
horchliam <53147246+horchliam@users.noreply.github.com>
2019-08-10 01:54:49 -0400

    Update twitter_bot.py

--- twitter_bot.py
+++ twitter_bot.py
@@ -75,6 +75,7 @@ def commentRandomTweets():
     time.sleep(2)
     pyautogui.mouseDown()
     pyautogui.mouseUp()
+    time.sleep(2)
     try:
         comment = browser.find_element_by_css_selector('div.r-3qxfft:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > svg:nth-child(2)')
         comment.click()
